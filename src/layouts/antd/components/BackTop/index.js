47fd38db5cc9acc6c54b273126c09f16583d0233
Héctor Patricio Poblete Concha <hepoblet@gmail.com>
2021-05-15 23:21:38 -0400

    refactor backtop

---
+++ src/layouts/antd/components/BackTop/index.js
@@ -0,0 +1,10 @@
+import * as AntDesign from 'antd';
+import { UpCircleFilled } from '@ant-design/icons';
+
+const BackTop = () => (
+  <AntDesign.BackTop>
+    <UpCircleFilled className="icon-back-top" />
+  </AntDesign.BackTop>
+);
+
+export default BackTop;
--- src/layouts/antd/index.js
+++ src/layouts/antd/index.js
@@ -1,21 +1,19 @@
 import 'antd/dist/antd.css';
 import './styles.css';
 import { element } from 'prop-types';
-import { Layout, BackTop } from 'antd';
-import { UpCircleFilled } from '@ant-design/icons';
+import { Layout } from 'antd';
 
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
+import BackTop from './components/BackTop';
 
 const AntDesignLayout = ({ children }) => (
   <Layout className="container">
     <Header />
     <Content>{children}</Content>
     <Footer />
-    <BackTop>
-      <UpCircleFilled className="icon-back-top" />
-    </BackTop>
+    <BackTop />
   </Layout>
 );
 
