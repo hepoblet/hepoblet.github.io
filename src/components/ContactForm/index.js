ae65061bb82655bb91ff2474fc81d169c348945d
Héctor Patricio Poblete Concha <hepoblet@gmail.com>
2021-05-16 22:57:56 -0400

    fix response form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -30,7 +30,7 @@ const ContactForm = () => {
         })
           .then((res) => res.json())
           .then((res) => {
-            if (response.ok) form.current.resetFields();
+            if (response?.ok) form.current.resetFields();
             setResponse(res);
             setLoading(false);
           });
