a4b79e4a9ed172e38853032c4b741c8b440fc064
Héctor Patricio Poblete Concha <hepoblet@gmail.com>
2021-06-04 02:18:27 -0400

    jest mock

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,16 @@
+/* eslint-disable react/prop-types */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+jest.mock('antd', () => {
+  const antd = jest.requireActual('antd');
+  const Menu = ({ children }) => <div>{children}</div>;
+  Menu.Item = ({ children }) => <div>{children}</div>;
+  return {
+    ...antd,
+    Menu,
+  };
+});
